8b0736e0354793509a0f78c0da26028e70a81fc7
nagasiwitch1 <nagasiwitch1@gmail.com>
2020-02-05 11:37:31 +0900

    modified 20200205 11:37

--- chart_making.py
+++ chart_making.py
@@ -7,13 +7,15 @@ from datetime import datetime as dt
 import datetime
 import matplotlib.dates as dates
 
-ticker1 = 'AAPL'
-path = './data/' + ticker1 + '.csv'
+ticker1 = 'DIS'
+path = './ib_db/' + 'DIS_20200205_3Y_1day' + '.csv'
+interval_num = 15
+
 
 raw_data = pd.read_csv(path)
 
-start_date = "2000-01-01"
-end_date = "2020-01-15"
+start_date = "2017-01-01"
+end_date = "2020-01-25"
 
 # to skip the days not in data
 sd = dt.strptime(start_date,"%Y-%m-%d")
@@ -37,19 +39,30 @@ else: pass
 
 raw_data1 = raw_data.set_index('Date')
 chart1 = raw_data1.loc[st:et]
-
+xlabel1 = pd.Series(chart1.index)
+print(xlabel1)
+xtick_interval = int((ed - sd).days / interval_num)
+xticklen = pd.Series(np.arange(0,len(xlabel1), xtick_interval)).astype(int)
+xticklen[interval_num-1] = 0
+xticklen.sort_values(inplace=True)
+xticklen.reset_index(drop=True, inplace=True)
+xlabel2 = xlabel1[xticklen]
+print(xtick_interval)
+print(xticklen)
+print(xlabel2)
 
 fig, ax = plt.subplots(figsize=(12,7))
 candlestick2_ohlc(ax,chart1.iloc[:,0], chart1.iloc[:,1], chart1.iloc[:,2],
                   chart1.iloc[:,3], width=0.6, colorup='g', colordown='r', alpha=0.75)
 
-ax.set_xlabel('Date')
-ax.set_ylabel(ticker1, color='g')
-ax.tick_params('y', colors='r')
-ax.plot(index=chart1.index)
-plt.legend(loc='upper left')
 
-xtick_interval = (ed - sd).days / 6
+ax.set_ylabel(ticker1, color='g')
+plt.xticks(ticks=xticklen, labels=xlabel2)
+ax.tick_params('y', colors='r')
 ax.xaxis.set_major_locator(ticker.MultipleLocator(xtick_interval))
+ax.tick_params('x', labelsize='small', labelrotation=45)
+ax.grid(which='major', axis='both', color='gray', dashes=(2, 4), linewidth=0.5)
+plt.margins(x=0, y=None, tight=True)
+
 fig.tight_layout()
 plt.show()
--- reqHistBar_ibinsync.py
+++ reqHistBar_ibinsync.py
@@ -3,12 +3,12 @@ import pandas as pd
 import datetime as dt
 # util.startLoop()  # uncomment this line when in a notebook
 
-symbol1 = "TSLA"
+symbol1 = "DIS"
 sectype = "STK"
 exchange1 = "SMART"
 enddatetime_temp = '20200205 00:00:00'
 duration1 = '3 Y'
-barsize1 = '2 hours'
+barsize1 = '1 day'
 
 
 # for timezone setting
