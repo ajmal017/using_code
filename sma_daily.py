68ffda4fe4e240fa44675becf7c17fa15ae73d18
nagasiwitch1 <nagasiwitch1@gmail.com>
2020-03-02 08:13:50 +0900

    delete

--- sma_daily.py
+++
@@ -1,9 +0,0 @@
-from pyalgotrade.technical import ma
-from pyalgotrade import plotter
-from pyalgotrade.barfeed import csvfeed
-
-feed = csvfeed.GenericBarFeed('DAY')
-fileName = "C:/python_final/using_codes/db/AAPL.csv"
-feed.addBarsFromCSV("AAPL", fileName)
-
-sma = ma.SMA()
