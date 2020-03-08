c48141e3f8c0a186f4d5762f114ec6b3d3e8c17d
nagasiwitch1 <59756713+nagasiwitch1@users.noreply.github.com>
2020-03-08 21:59:15 +0900

    Update dailyPNL.py

--- dailyPNL.py
+++ dailyPNL.py
@@ -14,7 +14,7 @@ df1 = util.df(account1)
 df2 = util.df(account2)
 df3 = util.df(account3)
 liquidity = int(pd.to_numeric(df1[df1['tag'] == 'NetLiquidationByCurrency'].iloc[0,2]))
-cash = int(pd.to_numeric(df1[df1['tag'] == 'CashBalance'].iloc[0, 2]))
+cash = round(pd.to_numeric(df1[df1['tag'] == 'CashBalance'].iloc[0, 2]) / liquidity * 100, 1)
 
 
 df2['contract'] = df2['contract'].astype(str)
@@ -27,8 +27,8 @@ for i in range(len(df2)):
 
 df_= df2[['position', 'averageCost', 'marketPrice', 'marketValue', 'unrealizedPNL', 'realizedPNL']]
 df_['Size'] = df_['marketValue'] / liquidity * 100
-df_['Unreal. P&L'] = df_['unrealizedPNL'] / liquidity * 100
-df_['Realized P&L'] = df_['realizedPNL'] / liquidity * 100
+df_['Unreal. P&L'] = df_['unrealizedPNL'] / df2['marketValue'] * 100
+df_['Realized P&L'] = df_['realizedPNL'] / df2['marketValue'] * 100
 df_['Daily Move'] = ''
 df_.insert(0, 'Symbol', df2['symbol'])
 
@@ -36,13 +36,10 @@ df = df_[['Symbol', 'Size', 'averageCost', 'marketPrice', 'Daily Move', 'Unreal.
 df.columns = ['Symbol', 'Size', 'Avg. Price', 'Last Price', 'Daily Move', 'Unreal. P&L', 'Realized P&L']
 df = round(df, 1)
 
-df3 = df3[['side', 'shares', 'price']]
-df3['Size'] = df3['shares'] * df3['price'] / liquidity * 100
+if df3 is not None:
+    df3 = df3[['side', 'shares', 'price']]
+    df3['Size'] = df3['shares'] * df3['price'] / liquidity * 100
 
-print(liquidity, df, df3)
-df.to_csv('daily.csv')
-
-
-# df2['symbol'] = df2['contract'].str.find('symbol=')
-#
-# print(df1, df2, liquidity, cash)
+print(df, "\n net liquid: ", liquidity, "\n action: ", df3, "\n daily Unreal PNL: ",
+      round((df_['unrealizedPNL'] / liquidity * 100).sum(), 1) , "\n daily Realized PNL: ",
+      round((df_['realizedPNL'] / liquidity * 100).sum(), 1) , "\n Cash: ", cash)
